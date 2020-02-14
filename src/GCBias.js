dab8a45572f9316af294eb35e60cedb89e5094d0
redpanda-cat <leungs1@mskcc.org>
2020-02-14 15:24:26 -0500

    Refactor GC Bias to new schema

--- src/GCBias.js
+++ src/GCBias.js
@@ -2,10 +2,11 @@ const { gql } = require("apollo-server");
 
 import _ from "lodash";
 import client from "./api/montageClient.js";
+import bodybuilder from "bodybuilder";
 var json = require("./gcbias.json");
 export const schema = gql`
   extend type Query {
-    gcBias(analysis: String, quality: String!): GCBias
+    gcBias(analysis: String, quality: String!, cellIDs: [String!]): GCBias
   }
   type GCBias {
     stats: GCStats
@@ -28,31 +29,66 @@ export const schema = gql`
 
 export const resolvers = {
   Query: {
-    async gcBias(_, { analysis, quality }) {
-      return await getGcBias(analysis, quality);
+    async gcBias(_, { analysis, quality, cellIDs }) {
+      return await getGcBias(analysis, quality, cellIDs);
     }
   },
   GCBias: {
-    stats: root => root.aggregations,
-    gcCells: root =>
-      root.hits.hits.filter(
-        hit => hit.fields.experimental_condition[0] === "GM"
-      )
+    stats: root => root["agg_stats_value"],
+    gcCells: root => root["agg_terms_gc_percent"]["buckets"]
   },
   GCCell: {
-    experimentalCondition: root => root.fields.experimental_condition[0],
-    gcPercent: root => root.fields.gc_percent[0],
-    highCi: root => root.fields.high_ci[0],
-    lowCi: root => root.fields.low_ci[0],
-    median: root => root.fields.median[0]
+    experimentalCondition: root => "",
+    gcPercent: root => root["key"],
+    highCi: root => {
+      const { avg, count, std_deviation } = root["agg_extended_stats_value"];
+      return avg + (1.96 * std_deviation) / Math.sqrt(count);
+    },
+    lowCi: root => {
+      const { avg, count, std_deviation } = root["agg_extended_stats_value"];
+      return avg - (1.96 * std_deviation) / Math.sqrt(count);
+    },
+    median: root => root["agg_percentiles_value"]["values"]["50.0"]
   },
+
   GCStats: {
-    yMax: root => root.yStats.max,
-    yMin: root => root.yStats.min,
-    xMax: root => root.xStats.max,
-    xMin: root => root.xStats.min
+    yMax: root => root.max,
+    yMin: root => root.min,
+    xMax: root => 0,
+    xMin: root => 100
   }
 };
-const getGcBias = async (analysis, quality) => {
-  return json;
-};
+async function getGcBias(analysis, quality, cellIDs) {
+  const cellIDQuery = bodybuilder()
+    .size(10000)
+    .filter("range", "quality", { gte: parseFloat(quality) })
+    .build();
+
+  const cellIDResults = await client.search({
+    index: `${analysis.toLowerCase()}_qc`,
+    body: cellIDQuery
+  });
+
+  const filteredCellIDs = cellIDs
+    ? cellIDs
+    : cellIDResults["body"]["hits"]["hits"].map(
+        record => record["_source"]["cell_id"]
+      );
+  const query = bodybuilder()
+    .size(0)
+    .filter("terms", "cell_id", filteredCellIDs)
+    .aggregation("terms", "gc_percent", { size: 200 }, a =>
+      a
+        .aggregation("extended_stats", "value")
+        .aggregation("percentiles", "value")
+    )
+    .aggregation("stats", "value")
+    .build();
+
+  const results = await client.search({
+    index: `${analysis.toLowerCase()}_gc_bias`,
+    body: query
+  });
+
+  return results["body"]["aggregations"];
+}
--- src/api/montageClient.js
+++ src/api/montageClient.js
@@ -2,7 +2,7 @@ const { Client } = require("@elastic/elasticsearch");
 
 const HOST = process.env.HOST || "localhost";
 const client = new Client({
-  node: "http://localhost:" + "2212"
+  node: "http://localhost:" + "9200"
   //  node: "http://frontend:2212"
 });
 
