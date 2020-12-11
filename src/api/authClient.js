2ccbb237017b2eaaa97d7979871a2ebf0fd4db87
redpanda-cat <leungs1@mskcc.org>
2020-12-11 14:51:32 -0500

    More flexible config for elasticsearch client

--- src/api/authClient.js
+++ src/api/authClient.js
@@ -5,10 +5,12 @@ const HOST = process.env.ELASTICSEARCH_HOST || process.env.HOST || "localhost";
 const PORT =
   process.env.ELASTICSEARCH_PORT || process.env.CLIENT_PORT || "2212";
 
+const URL = process.env.ELASTICSEARCH_NODE || "https://" + HOST + ":" + PORT;
+
 const authClient = (user, password) =>
   new Client({
     // node: "https://localhost:2212",
-    node: "https://" + HOST + ":" + PORT,
+    node: URL,
     auth: {
       username: user,
       password: password,
--- src/api/client.js
+++ src/api/client.js
@@ -3,9 +3,12 @@ const { Client } = require("@elastic/elasticsearch");
 const HOST = process.env.ELASTICSEARCH_HOST || process.env.HOST || "localhost";
 const PORT =
   process.env.ELASTICSEARCH_PORT || process.env.CLIENT_PORT || "2212";
+
+const URL = process.env.ELASTICSEARCH_NODE || "https://" + HOST + ":" + PORT;
+
 const client = (authKey, authKeyID) =>
   new Client({
-    node: "https://" + HOST + ":" + PORT,
+    node: URL,
     auth: {
       apiKey: {
         id: authKeyID,
