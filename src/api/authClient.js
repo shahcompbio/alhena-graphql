f5e61b71fc217de533feafa45ee2055ca0dd46fa
vbojilova <viki.bojilova@gmail.com>
2021-01-28 11:00:32 -0500

    Merging changes from sams branch

--- src/api/authClient.js
+++ src/api/authClient.js
@@ -1,11 +1,16 @@
+require("dotenv").config();
 const { Client } = require("@elastic/elasticsearch");
 
-const HOST = process.env.HOST || "localhost";
-const PORT = process.env.CLIENT_PORT || "2212";
+const HOST = process.env.ELASTICSEARCH_HOST || process.env.HOST || "localhost";
+const PORT =
+  process.env.ELASTICSEARCH_PORT || process.env.CLIENT_PORT || "2212";
+
+const URL = process.env.ELASTICSEARCH_NODE || "https://" + HOST + ":" + PORT;
+
 const authClient = (user, password) =>
   new Client({
-    node: "https://localhost:2212",
-    //  node: "https://" + HOST + ":" + PORT,
+    // node: "https://localhost:2212",
+    node: URL,
     auth: {
       username: user,
       password: password
--- src/api/redisClient.js
+++ src/api/redisClient.js
@@ -1,4 +1,8 @@
 var Redis = require("ioredis");
-var redis = process.env.HOST ? new Redis({ host: "redis" }) : new Redis({});
+var redis = process.env.REDIS_HOST
+  ? new Redis({ host: process.env.REDIS_HOST, port: process.env.REDIS_PORT })
+  : process.env.HOST
+  ? new Redis({ host: "redis" })
+  : new Redis({});
 
 export default redis;
